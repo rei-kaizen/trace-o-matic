5fa4093c1945e7ee64b52dabafdabd09bda15226
Rei Kaizen <bondenie@gmail.com>
2023-07-22 01:01:20 +0800

    modify and refactored programs

--- src/about_page.py
+++ src/about_page.py
@@ -7,11 +7,9 @@ class AboutWindow(Toplevel):
         self.title("About")
         self.geometry("1000x600")
 
-        # Load the image and resize it
         about_bg = Image.open("assets/about-p.png")
         about_bg = about_bg.resize((1000, 600), Image.LANCZOS)
 
-        # Display the image
         self.about_bg_image = ImageTk.PhotoImage(about_bg)
         self.about_bg_label = Label(self, image=self.about_bg_image)
         self.about_bg_label.pack()
--- src/faq_page.py
+++ src/faq_page.py
@@ -6,11 +6,9 @@ class FAQWindow(Toplevel):
         self.title("Frequently Asked Questions")
         self.geometry("1000x600")
 
-        # Load the image and resize it
         faqs_bg = Image.open("assets/faqs-p.png")
         faqs_bg = faqs_bg.resize((1000, 600), Image.LANCZOS)
 
-        # Display the image
         self.faqs_bgi = ImageTk.PhotoImage(faqs_bg)
         self.about_bg_label = Label(self, image=self.faqs_bgi)
         self.about_bg_label.pack()
